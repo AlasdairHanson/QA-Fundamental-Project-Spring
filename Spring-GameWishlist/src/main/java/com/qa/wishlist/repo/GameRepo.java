eba32b6963713aac779641907b33a86e693af21b
AlasdairHanson <AlasdairHanson@gmail.com>
2020-10-14 11:10:27 +0100

    Created repo

--- Spring-GameWishlist/src/main/java/com/qa/wishlist/repo/GameRepo.java
+++ Spring-GameWishlist/src/main/java/com/qa/wishlist/repo/GameRepo.java
@@ -1,5 +1,11 @@
 package com.qa.wishlist.repo;
 
-public class GameRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.wishlist.game.Game;
+
+@Repository
+public interface GameRepo extends JpaRepository<Game, Long> {
 
 }
