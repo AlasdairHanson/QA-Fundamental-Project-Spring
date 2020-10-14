9e43bc29b521b45f68a5a51d3e80810620521b79
AlasdairHanson <AlasdairHanson@gmail.com>
2020-10-14 11:06:17 +0100

    Created service

--- Spring-GameWishlist/src/main/java/com/qa/wishlist/service/GameService.java
+++ Spring-GameWishlist/src/main/java/com/qa/wishlist/service/GameService.java
@@ -1,5 +1,46 @@
 package com.qa.wishlist.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.wishlist.game.Game;
+import com.qa.wishlist.repo.GameRepo;
+
+@Service
 public class GameService {
+	
+	@Autowired
+	private GameRepo repo;
+
+	public GameService(GameRepo repo) {
+		super();
+		this.repo = repo;
+	}
+		
+	public Game createGame(Game game) {
+		return this.repo.save(game);
+	}
+
+	public List<Game> getGames() {
+		return this.repo.findAll();
+	}
+	
+	public Game updateGame(Game game, Long id) {
+		Game oldGame = this.repo.findById(id).orElseThrow();
+		
+		oldGame.setGameTitle(game.getGameTitle());
+		oldGame.setGamePrice(game.getGamePrice());
+		
+		Game saved = this.repo.save(oldGame);
+		return saved;
+	}
+
+	public boolean deleteGame(Long id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	}
+
 
 }
